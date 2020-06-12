cb33bbd364f8298ea1b753642e64444593c4a08b
Ahmed <asaad.memon@gmail.com>
2020-06-12 15:33:44 +0300

    corrected return bracket

--- src/dinner.js
+++ src/dinner.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function Dinner (){
-    return{
-        <h1>Today we are serving chicken biryani</h1>
-    }
+function Dinner() {
+    return(
+        <div>
+            <h1>Today we are serving chicken biryani</h1>
+            <h1>Today we are serving chicken Karhai</h1>    
+        </div>
+    )
 }
 
 export default Dinner;
