669a0f961c379db896474f1a87e4dffc1556037e
Ahmed <asaad.memon@gmail.com>
2020-06-12 15:22:03 +0300

    spacing {

--- src/dinner.js
+++ src/dinner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Dinner() {
-    return{
+    return {
         <div>
             <h1>Today we are serving chicken biryani</h1>
             <h1>Today we are serving chicken Karhai</h1>
