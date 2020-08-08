3fdfefabda3f5750d289fef0836296f73f6d1c63
Ahmed <asaad.memon@gmail.com>
2020-08-08 18:25:17 +0300

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dinner from './dinner';
 function App() {
   return (
    ///<div className="App">
-      <div>
+    <div>
       <Dinner dishName = "Chicken Biyani" sweet = "Kheer" />
     </div>
   );
